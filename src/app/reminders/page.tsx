087fd9124a54e36cb226836ff0badee6cb39630b
User <user@example.com>
2026-04-28 00:13:14 +0530

    Hardcode relative auth paths

--- src/app/reminders/page.tsx
+++ src/app/reminders/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "../../lib/auth";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth/next";
 import ApprovalForm from "./ApprovalForm";
